f1c302305aa246d3c5dc064ca23315175b6166fb
Marcin Krupnik <Marcin_Krupnik@epam.com>
2019-03-06 11:25:44 +0100

    Dodano funkcjonalnosc do autentykacji, wszystko dziala przez przegladarke

--- src/main/java/pl/mkrupnik/springtraining/SpringtrainingApplication.java
+++ src/main/java/pl/mkrupnik/springtraining/SpringtrainingApplication.java
@@ -2,12 +2,13 @@ package pl.mkrupnik.springtraining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 
 @SpringBootApplication
 public class SpringtrainingApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable{
 		SpringApplication.run(SpringtrainingApplication.class, args);
 	}
 

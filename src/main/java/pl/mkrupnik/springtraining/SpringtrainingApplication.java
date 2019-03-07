579a4dd13cf8e02cd46f3f885719be588b632f9a
Marcin Krupnik <Marcin_Krupnik@epam.com>
2019-03-07 12:22:59 +0100

    Sjf4j adnotacja dodana

--- src/main/java/pl/mkrupnik/springtraining/SpringtrainingApplication.java
+++ src/main/java/pl/mkrupnik/springtraining/SpringtrainingApplication.java
@@ -1,15 +1,18 @@
 package pl.mkrupnik.springtraining;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 
 @SpringBootApplication
+@Slf4j
 public class SpringtrainingApplication {
 
 	public static void main(String[] args) throws Throwable{
 		SpringApplication.run(SpringtrainingApplication.class, args);
+		log.info("Zaczynamy!");
 	}
 
 }
--- src/main/java/pl/mkrupnik/springtraining/controller/GeoController.java
+++ src/main/java/pl/mkrupnik/springtraining/controller/GeoController.java
@@ -1,17 +1,21 @@
 package pl.mkrupnik.springtraining.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.mkrupnik.springtraining.logging.LoggerService;
 import pl.mkrupnik.springtraining.service.GeoConsummer;
 import pl.mkrupnik.springtraining.geoobject.GeometryCollection;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class GeoController {
 
     @Autowired
@@ -21,14 +25,19 @@ public class GeoController {
     private GeoConsummer geoConsummer;
 
     @RequestMapping("/search")
-    public String geoController(@RequestParam(value="city") List<String> params) throws IOException {
+    public String geoController(HttpServletRequest request, @RequestParam(value="city") List<String> params) throws IOException {
         GeometryCollection result = new GeometryCollection();
         ObjectMapper mapper = new ObjectMapper();
+        StringBuilder sb = new StringBuilder();
+        String user = request.getRemoteUser();
+        sb.append(user + " requested: ");
             for (String s : params) {
                 int id = availableCities.getCityID(s);
                 GeometryCollection toAdd = mapper.readValue(geoConsummer.getGJSON(id), GeometryCollection.class);
                 result.add(toAdd.getMultiPolygon());
+                sb.append(s + ", ");
             }
+            log.info(sb.toString());
         return mapper.writeValueAsString(result);
     }
 }

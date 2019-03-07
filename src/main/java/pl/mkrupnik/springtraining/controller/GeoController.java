d3ea742b64dcbb62ca545b312fe5918436fe6b2d
Marcin Krupnik <Marcin_Krupnik@epam.com>
2019-03-07 15:28:16 +0100

    Zmieniono autoryzacje by dzialala na postmanie

--- src/main/java/pl/mkrupnik/springtraining/controller/GeoController.java
+++ src/main/java/pl/mkrupnik/springtraining/controller/GeoController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pl.mkrupnik.springtraining.logging.LoggerService;
 import pl.mkrupnik.springtraining.service.GeoConsummer;
 import pl.mkrupnik.springtraining.geoobject.GeometryCollection;
 
--- src/main/java/pl/mkrupnik/springtraining/security/WebSecurityConfig.java
+++ src/main/java/pl/mkrupnik/springtraining/security/WebSecurityConfig.java
@@ -17,15 +17,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers( "/", "/home").permitAll()
+                //.antMatchers( "/", "/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .httpBasic();
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
     }
 
     @Bean
